c55ef4c130b6e33571b5a1f9fd08d6eba5f28b0f
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-11-24 16:59:56 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,16 +8,7 @@ export const environment = {
     books: 'https://gutendex.com/books?ids=',
     dogImage: 'https://dog.ceo/api/breeds/image/random'
   },
-  firebase: {
-    apiKey: "AIzaSyC79D1lmkT-BMpwxhOD7Jvzx53YXI1BQQs",
-    authDomain: "b-749b4.firebaseapp.com",
-    projectId: "b-749b4",
-    storageBucket: "b-749b4.firebasestorage.app",
-    messagingSenderId: "616602352669",
-    appId: "1:616602352669:web:1b5f34ea8beda23b1b9af4",
-    measurementId: "G-LJEG66DC5J"
-    
-  }
+  
 };
 
 /*
